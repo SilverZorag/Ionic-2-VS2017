18b0a3917aba190dc9f74ae57cdd14ef7573784a
SilverZorag <fabrizio9493@gmail.com>
2017-04-04 13:58:58 +0200

    update 2

--- App Meteo/App Meteo/src/app/app.module.ts
+++ App Meteo/App Meteo/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+﻿import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { WeatherProvider } from '../providers/weather';
 @NgModule({
   declarations: [
     MyApp,
@@ -16,6 +16,6 @@ import { HomePage } from '../pages/home/home';
     MyApp,
     HomePage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [WeatherProvider, {provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}
